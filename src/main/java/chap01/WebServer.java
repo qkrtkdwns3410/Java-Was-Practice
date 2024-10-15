eec71f575b491e88e8181f7ac1a2f9c39221e87e
박상준 <sjunpark@itembay.co.kr>
2024-10-15 17:57:05 +0900

    dd

--- src/main/java/chap01/WebServer.java
+++ src/main/java/chap01/WebServer.java
@@ -20,31 +20,31 @@ public class WebServer {
         try {
             ServerSocket server = new ServerSocket(8001);
             System.out.println("클라이언트로부터 접속을 기다리고 있습니다.");
-            
+
             File receiveFile = new File("src/main/resources/chap01/server_receive.txt");
             File sendFile = new File("src/main/resources/chap01/server_send.txt");
-            
+
             FileOutputStream fos = new FileOutputStream(receiveFile);
             FileInputStream fis = new FileInputStream(sendFile);
-            
+
             Socket socket = server.accept();
             System.out.println("클라이언트 접속 완료");
-            
+
             int ch;
             InputStream in = socket.getInputStream();
-            
+
             // 클라이언트로부터 받은 데이터를 파일로 저장
             while ((ch = in.read()) != 0) {
                 fos.write(ch);
             }
-            
+
             // server_send.txt 파일의 내용을 클라이언트로 전송
             OutputStream os = socket.getOutputStream();
-            
+
             while ((ch = fis.read()) != -1) {
                 os.write(ch);
             }
-            
+
             socket.close();
             System.out.println("클라이언트와 통신 종료");
         } catch (Exception e) {
