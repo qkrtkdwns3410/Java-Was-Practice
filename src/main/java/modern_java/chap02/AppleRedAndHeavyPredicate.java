92f802a3b361bd51e304d60d15155ef60eb1ccd6
박상준 <80584060+qkrtkdwns3410@users.noreply.github.com>
2024-10-18 22:57:33 +0900

    엔진엑스 통한 클라이언트 서버 통신

---
+++ src/main/java/modern_java/chap02/AppleRedAndHeavyPredicate.java
@@ -0,0 +1,13 @@
+package modern_java.chap02;
+
+/**
+ * packageName    : modern_java.chap02
+ * fileName       : AppleRedAndHeavyPredicate
+ * author         : ipeac
+ * date           : 24. 10. 18.
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 24. 10. 18.        ipeac       최초 생성
+ */
---
+++ src/main/java/modern_java/chap02/변화하는요구사항대응하기.java
@@ -0,0 +1,149 @@
+package modern_java.chap02;
+
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * packageName    : modern_java.chap02
+ * fileName       : 변화하는요구사항대응하기
+ * author         : ipeac
+ * date           : 24. 10. 18.
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 24. 10. 18.        ipeac       최초 생성
+ */
+public class 변화하는요구사항대응하기 {
+    public static void main(String[] args) {
+        List<Apple> inventories = List.of(
+                new Apple(Color.RED, 100),
+                new Apple(Color.GREEN, 200),
+                new Apple(Color.RED, 300),
+                new Apple(Color.GREEN, 400)
+        );
+        
+        prettyPrintApple(inventories, new ApplePrettyPrintFormatter());
+    }
+    
+    public static void prettyPrintApple(List<Apple> inventories, AppleFormatter predicate) {
+        for (Apple apple : inventories) {
+            String message = predicate.accept(apple);
+            
+            System.out.println(message);
+        }
+    }
+    
+    public interface AppleFormatter {
+        String accept(Apple apple);
+    }
+    
+    public static class ApplePrettyPrintFormatter implements AppleFormatter {
+        @Override
+        public String accept(Apple apple) {
+            String characteristic = apple.getWeight() > 150 ? "heavy" : "light";
+            
+            return MessageFormat.format("{0} apple with {1} weight", apple.getColor(), characteristic);
+        }
+    }
+    
+
+    
+    public static List<Apple> filterGreenApples(List<Apple> inventory) {
+        List<Apple> result = new ArrayList<>();
+        
+        for (Apple apple : inventory) {
+            if (apple.getColor() == Color.GREEN) {
+                result.add(apple);
+            }
+        }
+        
+        return result;
+    }
+    
+    public static List<Apple> filterRedApplesByColor(List<Apple> inventory) {
+        List<Apple> result = new ArrayList<>();
+        
+        for (Apple apple : inventory) {
+            if (apple.getColor() == Color.RED) {
+                result.add(apple);
+            }
+        }
+        return result;
+    }
+    
+    //무게 기준 추가
+    public static List<Apple> filterApplesByWeight(List<Apple> inventory, int weight) {
+        List<Apple> result = new ArrayList<>();
+        
+        for (Apple apple : inventory) {
+            if (apple.getWeight() > weight) {
+                result.add(apple);
+            }
+        }
+        
+        return result;
+    }
+    
+    //동작 파라미터화
+    public static List<Apple> filterApples(List<Apple> inventory, ApplePredicate predicate) {
+        List<Apple> result = new ArrayList<>();
+        
+        for (Apple apple : inventory) {
+            if (predicate.test(apple)) {
+                result.add(apple);
+            }
+        }
+        
+        return result;
+    }
+    
+    static class Apple {
+        private Color color;
+        
+        private int weight;
+        
+        public Apple(Color color, int weight) {
+            this.color = color;
+            this.weight = weight;
+        }
+        
+        public int getWeight() {
+            return weight;
+        }
+        
+        public Color getColor() {
+            return color;
+        }
+        
+        @Override
+        public String toString() {
+            return "Apple{" + "color=" + color +
+                    ", weight=" + weight +
+                    '}';
+        }
+    }
+    
+    enum Color {
+        RED, GREEN
+    }
+    
+    public interface ApplePredicate {
+        boolean test(Apple apple);
+    }
+    
+    public static class AppleRedAndHeavyPredicate implements ApplePredicate {
+        @Override
+        public boolean test(Apple apple) {
+            return apple.getWeight() > 150 && apple.getColor() == Color.RED;
+        }
+    }
+    
+    public static class AppleGreenColorPredicate implements ApplePredicate {
+        @Override
+        public boolean test(Apple apple) {
+            return apple.getColor() == Color.GREEN;
+        }
+    }
+}
